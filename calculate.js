d5918cc38ac38fc010a06715ce8256c0ed051edb
falgunmahajan <falgunmahajan1999@gmail.com>
2023-05-18 16:47:45 +0530

    Initial commit

---
+++ calculate.js
@@ -0,0 +1,17 @@
+const calculate = document.querySelector(".btn");
+const range = document.querySelector("#tip-percentage");
+calculate.addEventListener("click", () => {
+    tipAmount();
+    document.querySelector(".result").style.display = "block"
+})
+range.addEventListener("change",tipAmount)
+
+function tipAmount() {
+    
+    const amount = document.querySelector("#amount").value;
+    const tipPercentage = range.value;
+    document.querySelector(".tip").innerHTML = `${tipPercentage}%`;
+    const tip = amount * tipPercentage / 100;
+    document.querySelector("#Original-amount").value = amount;
+    document.querySelector("#Tip-amount").value = tip;
+}
