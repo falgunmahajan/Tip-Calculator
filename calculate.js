e9af9514072ea95e8552de5bc585f0e6308b1e5c
falgunmahajan <falgunmahajan1999@gmail.com>
2023-05-22 16:16:45 +0530

    Tip calculator

--- calculate.js
+++ calculate.js
@@ -1,14 +1,16 @@
 const calculate = document.querySelector(".btn");
 const range = document.querySelector("#tip-percentage");
+const input=document.querySelector("#amount")
 calculate.addEventListener("click", () => {
     tipAmount();
-    document.querySelector(".result").style.display = "block"
+    document.querySelector(".result").style.display = "block";
+    calculate.style.display="none";
 })
 range.addEventListener("change",tipAmount)
 
 function tipAmount() {
     
-    const amount = +(document.querySelector("#amount").value);
+    const amount = +(input.value);
     const tipPercentage = range.value;
     document.querySelector(".tip").innerHTML = `${tipPercentage}%`;
     
@@ -16,3 +18,4 @@ function tipAmount() {
     document.querySelector("#Original-amount").value = amount;
     document.querySelector("#Tip-amount").value = tip;
 }
+input.addEventListener("keyup",tipAmount);
